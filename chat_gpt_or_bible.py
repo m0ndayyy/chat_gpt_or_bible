cf281c567289f29326143f0d8440544a4fe8f014
Ross Shaffer <ross.g.shaffer@gmail.com>
2023-09-04 18:06:38 -0400

    update to python file

--- chat_gpt_or_bible.py
+++ chat_gpt_or_bible.py
@@ -1,4 +1,21 @@
 import openai
 import pythonbible
+import random
 
-print("Hello.")
+
+openai.api_key = "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"
+messages = [{"role":"system", "content":"You will respond with the same style that the bible is written in."}]
+messages.append({"role":"user", "content":"please generate a fake bible verse as if could be found somewhere in the bible."})
+chat = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=messages)
+reply = chat.choices[0].message.content
+print(reply)
+
+
+book = random.randint(1, 66)
+max_chapters = pythonbible.get_number_of_chapters(pythonbible.Book(book))
+chapter = random.randint(1, max_chapters)
+max_verses = pythonbible.get_number_of_verses(pythonbible.Book(book), chapter)
+verse = random.randint(1, max_verses)
+quote_id = pythonbible.get_verse_id(pythonbible.Book(book), chapter, verse)
+quote = pythonbible.get_verse_text(quote_id)
+print(quote)
